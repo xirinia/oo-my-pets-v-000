70eb57c123542f61775f1b43604293851ec12005
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-13 04:21:39 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,4 +31,14 @@ class Owner
     @pets[:fishes] << fish
   end
 
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
 end
